fe3fa8fe4159f2f3fea5303403f0e4cc1eec6295
Will Wright <92600136+gitveryown@users.noreply.github.com>
2023-09-27 11:39:09 -0400

    initailizer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shop';
+  title = 'wishlist';
+
+  items : WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ]
+  
+  toggleItem(item: WishItem){
+    item.isComplete = !item.isComplete
+    console.log(item)
+  }
+  
+  newWishItem = ''
+  
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishItem))
+    this.newWishItem = ''
+  }
+  
+  
 }
